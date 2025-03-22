ed37f41da095a913d3bac82a9210afd6b07f0f6b
Jonghoon <songwoobum@naver.com>
2025-03-22 22:01:40 +0900

    create project

---
+++ GithubActions/GithubActions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubActions
+//
+//  Created by Jaxtyn on 3/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ GithubActions/GithubActionsTests/GithubActionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsTests.swift
+//  GithubActionsTests
+//
+//  Created by Jaxtyn on 3/22/25.
+//
+
+import Testing
+@testable import GithubActions
+
+struct GithubActionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
